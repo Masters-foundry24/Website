e6d030a9d799d83f7156a7c6ef08b14b5974df7f
Masters Foundry <ventures.masters@gmail.com>
2024-04-25 10:16:51 +0000

    reAtualizando
    teste

---
+++ public/js/cart.js
@@ -0,0 +1,23 @@
+
+
+const payBtn=document.querySelector(".btn-buy");
+
+
+
+payBtn.addEventListener("click", ()=>{
+    fetch("http://127.0.0.1:3000/strip-checkout",{
+     method: "post",
+     headers: new Headers({"Content-Type": "application/json"}), // Corrigido aqui
+     body: JSON.stringify({
+     items: JSON.parse(localStorage.getItem("cartItems")),
+     }),
+    })
+    .then((res)=>res.json())
+    .then((url)=>{
+     location.href=url;
+     clearCart();
+    })
+    .catch((err)=> console.log(err));
+ });
+ 
+ 
